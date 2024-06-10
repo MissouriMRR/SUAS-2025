f8a9d27b4ea93951f34cb14b2707ff286ebb3e1b
Ooglely <alennavdic@gmail.com>
2024-06-09 22:16:13 -0500

    Fix odlc contour detection and pre-commit issues

--- vision/standard_object/odlc_contour_detection.py
+++ vision/standard_object/odlc_contour_detection.py
@@ -1,9 +1,31 @@
+"""Implements contour filtering to return contours that are likely to be shapes we expect to see."""
+
+# pylint: disable=too-many-locals
+
 import cv2
 import numpy as np
 
+from vision.common.constants import Contour, Hierarchy, Image
 
-def fetchShapeContours(
-    filename: str, draw_contours: bool = False, resulting_file_name: str = ""
+# Contour restriction constants
+MAX_CONTOUR_AREA: int = 10000
+MIN_CONTOUR_AREA: int = 300
+MAX_ASPECT_RATIO: float = 3
+MIN_SOLIDITY: float = 0.75
+MIN_PROPORTIONAL_AREA: float = 0.4
+
+# Contour detection constants
+KERNEL_SIZE: cv2.typing.Size = (5, 5)
+MIN_WHITE_VALUE: int = 195
+MAX_BLACK_VALUE: int = 60
+MIN_SATURATION_VALUE: int = 50
+MIN_DARK_SATURATION_VALUE: int = 125
+BLUR_IMG_WEIGHT: float = 0.7
+NORMAL_IMG_WEIGHT: float = 0.3
+
+
+def fetch_shape_contours(
+    image: Image, draw_contours: bool = False, resulting_file_name: str = ""
 ) -> list[np.ndarray[np.dtype[np.uint8], np.dtype[np.uint8]]]:
     """
     Detects the boundaries of potential shapes based on a pixel or region's
@@ -26,9 +48,10 @@ def fetchShapeContours(
             an array of all points that make up the contour
     """
 
-    img = cv2.imread(filename)
-    new_img = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)  # converts image to HLS color format
-    new_img = cv2.GaussianBlur(new_img, (5, 5), sigmaX=0, sigmaY=0)  # blurs HLS image
+    img = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)  # converts image to HLS color format
+
+    # Blur is added to the image to reduce noise and to make the image/grass more uniform
+    new_img = cv2.GaussianBlur(img, KERNEL_SIZE, sigmaX=0, sigmaY=0)  # blurs HLS image
 
     img_brightness: np.ndarray[np.dtype[np.uint8], np.dtype[np.uint8]]
     img_saturation: np.ndarray[np.dtype[np.uint8], np.dtype[np.uint8]]
@@ -37,8 +60,12 @@ def fetchShapeContours(
     img_saturation = np.array(new_img[:, :, 2])  # reads saturation of image as 2D array
 
     # slightly blends blurred and unblurred images of same type, prioritizing blurred images
-    img_brightness = cv2.addWeighted(new_img[:, :, 1], 0.3, img_brightness, 0.7, 0)
-    img_saturation = cv2.addWeighted(new_img[:, :, 2], 0.3, img_saturation, 0.7, 0)
+    img_brightness = cv2.addWeighted(
+        img[:, :, 1], NORMAL_IMG_WEIGHT, img_brightness, BLUR_IMG_WEIGHT, 0
+    )
+    img_saturation = cv2.addWeighted(
+        img[:, :, 2], NORMAL_IMG_WEIGHT, img_saturation, BLUR_IMG_WEIGHT, 0
+    )
 
     avg_brt: float = float(np.average(img_brightness))  # gets average brightness
 
@@ -46,54 +73,57 @@ def fetchShapeContours(
     black_thresh: np.ndarray[np.dtype[np.uint8], np.dtype[np.uint8]]
     saturation_thresh: np.ndarray[np.dtype[np.uint8], np.dtype[np.uint8]]
 
-    # gets all values with a brightness greater than 195, less than 60,
-    # and with a saturation of greater than 50 (or 125 if the image is excessively dark)
-    _, white_thresh = cv2.threshold(img_brightness, 195, 255, cv2.THRESH_BINARY)
-    _, black_thresh = cv2.threshold(img_brightness, 60, 255, cv2.THRESH_BINARY_INV)
-    _, saturation_thresh = cv2.threshold(
-        img_saturation, (50 if avg_brt > 60 else 125), 255, cv2.THRESH_BINARY
-    )
+    # Use higher saturation value if image is bright, lower if dark
+    saturation_value: int = MIN_SATURATION_VALUE if avg_brt > 60 else MIN_DARK_SATURATION_VALUE
 
-    # expands each threshold by 3 pixels in the x and y direction to merge those in close proximity to one another
+    # White threshold is used to find the brightest parts of the image
+    # Black threshold is used to find the darkest parts of the image
+    # Saturation threshold is used to find the most saturated parts of the image
+    _, white_thresh = cv2.threshold(img_brightness, MIN_WHITE_VALUE, 255, cv2.THRESH_BINARY)
+    _, black_thresh = cv2.threshold(img_brightness, MAX_BLACK_VALUE, 255, cv2.THRESH_BINARY_INV)
+    _, saturation_thresh = cv2.threshold(img_saturation, saturation_value, 255, cv2.THRESH_BINARY)
+
+    # expands each threshold by 3 pixels in the x and y direction
+    # to merge those in close proximity to one another
     white_thresh = cv2.dilate(white_thresh, np.ones([3, 3], np.uint8))
     black_thresh = cv2.dilate(black_thresh, np.ones([3, 3], np.uint8))
     saturation_thresh = cv2.dilate(saturation_thresh, np.ones([3, 3], np.uint8))
 
-    contours: np.ndarray[np.dtype[np.uint8], np.dtype[np.uint8]]
+    contours: list[Contour]
+    _hierarchy: Hierarchy
 
     # finds the contour outlines of the combined thresholds
-    contours, _ = cv2.findContours(
+    contours, _hierarchy = cv2.findContours(
         (white_thresh + black_thresh + saturation_thresh), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
     )
-
-    all_contours: list[np.ndarray[np.dtype[np.uint8], np.dtype[np.uint8]]] = []
-    for a in contours:
+    all_contours: list[Contour] = []
+    for contour in contours:
         # gets rectangle bounding entire contour
-        x, y, w, h = cv2.boundingRect(a)
-        area = float(cv2.contourArea(a))
+        _x_pos, _y_pos, width, height = cv2.boundingRect(contour)
+        area = float(cv2.contourArea(contour))
 
         # calculates area inside contour in proportion to the area of the bounding rectangle
-        proportional_area = area / (w * h)
-        aspect_ratio = max(float(w) / h, float(h) / w)
+        proportional_area = area / (width * height)
+        aspect_ratio = max(float(width) / height, float(height) / width)
 
         # calculates solidity/rigidity of shape (shapes with rougher sides have lower solidity)
-        solidity = area / (cv2.contourArea(cv2.convexHull(a)))
+        solidity = area / (cv2.contourArea(cv2.convexHull(contour)))
 
         # saves the contour if the area is a reasonable size, reasonably close to a square,
         # is not extremely small compared to its bounding box, and does not have very rough edges.
         if (
-            (10000 >= cv2.contourArea(a) >= 300)
-            and (aspect_ratio <= 3)
-            and (proportional_area >= 0.4)
-            and (solidity >= 0.75)
+            (MAX_CONTOUR_AREA >= cv2.contourArea(contour) >= MIN_CONTOUR_AREA)
+            and (aspect_ratio <= MAX_ASPECT_RATIO)
+            and (proportional_area >= MIN_PROPORTIONAL_AREA)
+            and (solidity >= MIN_SOLIDITY)
         ):
-            all_contours.append(a)
+            all_contours.append(contour)
 
     # draws contours and writes to output image file (but only if specified)
     if draw_contours:
-        for a in all_contours:
-            cv2.drawContours(img, [a], 0, (0, 0, 255), 2)
-        cv2.imwrite(resulting_file_name, img)
+        for contour in all_contours:
+            cv2.drawContours(image, [contour], 0, (0, 0, 255), 2)
+        cv2.imwrite(resulting_file_name, image)
 
     # returns a filtered list of contours
     return all_contours
