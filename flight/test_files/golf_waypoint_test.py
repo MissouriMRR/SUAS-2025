0b220244c195ed7f80c45327bf35a59f00b288b8
RexBerry <59031902+RexBerry@users.noreply.github.com>
2024-10-08 18:35:54 -0500

    Use the Drone class in the golf waypoint test

--- flight/test_files/golf_waypoint_test.py
+++ flight/test_files/golf_waypoint_test.py
@@ -5,11 +5,11 @@ Main driver code for moving drone to each waypoint
 import asyncio
 import logging
 import sys
-from typing import List
 
-from dronekit import Vehicle, connect, VehicleMode
+from dronekit import VehicleMode
 
 from flight.waypoint.goto import move_to
+from state_machine.drone import Drone
 
 SIM_ADDR: str = "tcp:127.0.0.1:5762"  # Address to connect to the simulator
 CONTROLLER_ADDR: str = "/dev/ttyFTDI"  # Address to connect to a pixhawk board
@@ -32,49 +32,50 @@ async def run(sim: bool) -> None:
     specified by the latitude and longitude and continues to run until forced disconnect
     """
     # Put all latitudes, longitudes and altitudes into separate arrays
-    lats: List[float] = [37.948658, 37.948200, 37.948358, 37.948800]
-    longs: List[float] = [-91.784431, -91.783406, -91.783253, -91.784169]
+    lats: list[float] = [37.948658, 37.948200, 37.948358, 37.948800]
+    longs: list[float] = [-91.784431, -91.783406, -91.783253, -91.784169]
 
     # create a drone object
     address: str = SIM_ADDR if sim else CONTROLLER_ADDR
     logging.info("Waiting for drone to connect...")
-    drone: Vehicle = connect(address, baud=921600, wait_ready=True)
+    drone: Drone = Drone(address, baud=921600)
+    await drone.connect_drone()
     logging.info("Drone discovered!")
 
     # initilize drone configurations
-    drone.airspeed = 10
+    drone.vehicle.airspeed = 10
 
     # connect to the drone
     logging.info("Waiting for pre-arm checks to pass...")
-    while not drone.is_armable:
+    while not drone.vehicle.is_armable:
         await asyncio.sleep(0.5)
 
     logging.info("-- Arming")
-    drone.mode = VehicleMode("GUIDED")
-    drone.armed = True
-    while drone.mode != "GUIDED" or not drone.armed:
+    drone.vehicle.mode = VehicleMode("GUIDED")
+    drone.vehicle.armed = True
+    while drone.vehicle.mode != "GUIDED" or not drone.vehicle.armed:
         await asyncio.sleep(0.5)
 
     logging.info("-- Taking off")
-    drone.simple_takeoff(15)
+    drone.vehicle.simple_takeoff(15)
 
     # wait for drone to take off
-    while drone.location.global_relative_frame.alt < 14.9:
+    while drone.vehicle.location.global_relative_frame.alt < 14.9:
         await asyncio.sleep(1)
 
     # move to each waypoint in mission
     for i in range(2):
         logging.info("Starting loop %s", i)
         for point in range(len(lats)):
-            await move_to(drone, lats[point], longs[point], 15)
+            await move_to(drone.vehicle, lats[point], longs[point], 15)
 
     # return home
     logging.info("Last waypoint reached")
     logging.info("Returning to home")
-    drone.mode = VehicleMode("RTL")
-    while drone.mode.name != "RTL":
+    drone.vehicle.mode = VehicleMode("RTL")
+    while drone.vehicle.mode.name != "RTL":
         await asyncio.sleep(0.5)
-    while drone.system_status.state != "STANDBY":
+    while drone.vehicle.system_status.state != "STANDBY":
         await asyncio.sleep(0.5)
     print("Staying connected, press Ctrl-C to exit")
 
